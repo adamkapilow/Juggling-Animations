f6cfdc7c52392d4496acb6de37df788d9a0e6e64
adamkapilow <adamkapilow@gmail.com>
2022-10-27 14:52:17 -0700

    Added comments

--- rotating_polygon.py
+++ rotating_polygon.py
@@ -1,14 +1,30 @@
 from manim import *
 import numpy as np
 
+"""
+This file contains functions and scenes concering decorating
+a rotating polygon with labels and arrows.
+"""
+
 #uncomment to set default colors to black, on a white background
 """ 
+confing.camera.background_color = WHITE
 Text.set_default(color=BLACK)
 Arrow.set_default(color=BLACK)
 NumberLine.set_default(color=BLACK)
 DecimalNumber.set_default(color=BLACK) """
 
 
+"""
+This function returns a loop with an arrow attached to a given vertex of a polygon.
+Inputs:
+throw_index -- Integer less than the number of sides in the polygon, 
+               specifying which vertex to base the loop at
+polygon -- a Polygon object
+loop_radius -- A float specifying the radius of the loop
+Returns:
+VGroup of the loop, a ParametricFunction, and a Polygon acting as the triangular tip.
+"""
 def get_loop(throw_index, polygon, loop_radius):
     angle_remaining = 0.1*TAU
     start = polygon.get_vertices()[throw_index]
@@ -16,8 +32,8 @@ def get_loop(throw_index, polygon, loop_radius):
     center = start + (loop_radius/np.linalg.norm(center_to_start))*(center_to_start)
     start_angle = angle_of_vector(center_to_start) - PI
     end_angle = start_angle + TAU - angle_remaining
-    def circle_param(t, radius=loop_radius):
-        return center + radius*np.array([np.cos(t), np.sin(t), 0])
+    def circle_param(t):
+        return center + loop_radius*np.array([np.cos(t), np.sin(t), 0])
     loop = ParametricFunction(circle_param, t_range=[start_angle, end_angle])
     altitude = start - circle_param(end_angle)
     tip_displacement = np.array([altitude[1], -altitude[0], 0])/np.sqrt(3)
@@ -26,6 +42,13 @@ def get_loop(throw_index, polygon, loop_radius):
     tip = Polygon(start, vertex1, vertex2, fill_opacity=1, color=loop.color)
     return VGroup(loop, tip)
 
+"""
+Returns a CurvedArrow pointing between two specified vertices on a polygon.
+Inputs:
+throw_index -- Tail of CurvedArrow
+catch_index -- Tip of CurvedArrow
+polygon -- Polygon object to attach to
+"""
 def get_arc(throw_index, catch_index, polygon):
     start = polygon.get_vertices()[throw_index]
     end = polygon.get_vertices()[catch_index]
@@ -34,7 +57,20 @@ def get_arc(throw_index, catch_index, polygon):
     arc.catch_index = catch_index
     return CurvedArrow(start, end, angle=-TAU/4)
 
-
+"""
+Sets up a scene to have a polygon with prescribed labels on the vertices, with or without arcs drawn between vertices, to other specified parameters. The polygon's vertices start pointing north, increasing in index moving clockwise.
+Inputs:
+scene -- A Scene object to render to
+N -- number of vertices for the polygon
+polygon_radius -- Optional, the radius of the circumcircle for the polygon.
+label_values -- Optional, a list of values for labels on the vertices of the polygon.
+fixed_labels -- Optional, boolean whether to include an additional set of labels 
+                which don't move with the polygon.
+draw_arcs -- Optional boolean, whether to draw arcs on the polygon, only to be used when the label values are given as throw heights in a siteswap.
+Returns:
+Nothing, but adds attributes scene.polygon, scene.moving_labels, scene.fixed_labels, scene.arcs 
+to scene, also adds all these objects to the scene, displaying them.
+"""
 def rotation_scene(scene : Scene, N : int, polygon_radius=1.5, label_values = None, fixed_labels=True, draw_arcs=False, loop_radius=0.5):
     if label_values == None:
         label_values = list(range(N))
@@ -82,18 +118,35 @@ def rotation_scene(scene : Scene, N : int, polygon_radius=1.5, label_values = No
             scene.fixed_labels.append(fixed_label)
             scene.add(fixed_label)
     scene.add(scene.polygon)
-            
+
+"""
+A wrapped for rotation_scene, used specifically to draw a siteswap on a polygon.
+Inputs:
+scene -- A Scene object to render to
+throw_heights -- A list of integers specifying the throw heights in a juggling pattern
+polygon_radius -- Optional, radius of the circumcircle of the polygon 
+                  containing the pattern diagram.
+loop_radius -- Optional, radius of any loops in the siteswap diagram, 
+               arrows landing on the same beat they were thrown at, corresponding to
+               throw heights that are a multiple of the period.
+"""
 def draw_siteswap(scene, throw_heights, polygon_radius=1.5, loop_radius=0.5):
     rotation_scene(scene, len(throw_heights), polygon_radius=polygon_radius, 
                    label_values=throw_heights, fixed_labels=False, 
                    draw_arcs=True, loop_radius=0.5)
 
 
+"""
+A scene depicting the siteswap 531, and showing its invariance under rotation. 
+"""
 class fivethreeone(Scene):
     def construct(self):
         draw_siteswap(self, [5, 3, 1])
         self.play(Rotate(self.polygon))
 
+"""
+A scene that rotates a twelve hour clock one hour forward.
+"""
 class rotation_twelve_once(Scene):
     def construct(self):
        self.camera.background_color=WHITE
@@ -102,7 +155,9 @@ class rotation_twelve_once(Scene):
        self.wait(5)
 
 
-        
+"""
+A scene that rotates a twelve hour clock 4 hours forward.
+"""
 class rotation_twelve_four(Scene):
     def construct(self):
         self.camera.background_color=WHITE
@@ -110,6 +165,9 @@ class rotation_twelve_four(Scene):
         self.play(Rotate(self.polygon, -4*2*np.pi/12, run_time=3))
         self.wait(5)
 
+"""
+A scene that rotates a twelve hour clock one hour backward.
+"""
 class rotation_twelve_back(Scene):
     def construct(self):
         self.camera.background_color=WHITE
@@ -118,6 +176,11 @@ class rotation_twelve_back(Scene):
         self.play(Rotate(self.polygon, 4*np.pi/12, run_time=3))
         self.wait(3)
 
+
+"""
+A scene that rotates a twelve hour clock forward twelve hours, done
+one hour at a time, keeping track of the elapsed rotation.
+"""
 class rotation_twelve_twelve(Scene):
     def construct(self):
         self.camera.background_color = WHITE
@@ -133,6 +196,11 @@ class rotation_twelve_twelve(Scene):
             self.wait(0.5)
         self.wait(3)
 
+
+"""
+A scene demonstrating the fact that for each tickmark T on the clock,
+there is a unique rotation of the clock sending the top tickmark to T.
+"""
 class tick_map(Scene):
     def construct(self):
         self.camera.background_color=WHITE
@@ -153,7 +221,10 @@ class tick_map(Scene):
 
 
         
-
+"""
+A scene demonstrating the fact that the sliding action on the number line
+sending 0 to 4 corresponds to the rotating action on the clock sending time 0 to time 4.
+"""
 class slide_and_rotate(Scene):
     def construct(self):
         self.camera.background_color = BLACK
@@ -197,6 +268,10 @@ class slide_and_rotate(Scene):
         self.play(time.animate.set_value(4), run_time=4)
         self.wait(4)
 
+"""
+A scene demonstrating the fact that addition on the number line corresponds to
+composition of rotating actions on the clock.
+"""
 class homomorphism(Scene):
     def construct(self):
         Text.set_default(color=WHITE)
@@ -234,10 +309,14 @@ class homomorphism(Scene):
         self.wait(2)
         self.play(time.animate.set_value(5), run_time=3)
         self.wait(6)
-        
+
+"""
+A scene demonstrating the fact that for any number x on the number line,
+there is a unique sliding action sending 0 to x.
+"""
 class slide(Scene):
     def construct(self):
-        self.camera.background_color = WHITE
+        #self.camera.background_color = WHITE
         line = NumberLine(x_range=[-12,12], include_numbers=True, font_size=75)
         line2 = NumberLine(x_range=[-8,8], include_numbers=True, font_size=75)
         line.shift(0.8*UP)
@@ -259,12 +338,13 @@ class slide(Scene):
         self.play(anims)
         self.wait(6)
 
+"""
+A scene depicting the siteswap 6451, showing that it is invariant under rotation.
+"""
 class sixfourfiveone(Scene):
     def construct(self):
-        self.camera.background_color = WHITE
+        #self.camera.background_color = WHITE
         draw_siteswap(self, [6, 4, 5, 1], polygon_radius=2.3)
         self.wait()
         self.play(Rotate(self.polygon, -3*TAU/4, OUT, self.polygon.get_center()), run_time=3)
         self.wait(6)
-
-config.background_color = BLACK
